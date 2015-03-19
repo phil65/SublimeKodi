6e720de1d5f615d1b9f1061165a848a9a76e815c
Philipp Te <philipptemminghoff@gmx.de>
2015-03-19 02:20:25 +0100

    started with settings + kodi core labels

--- koditranslatelabels.py
+++ koditranslatelabels.py
@@ -6,18 +6,17 @@ import os
 
 class KodiTranslatedLabelToolTip(sublime_plugin.EventListener):
 
-    def on_activate(self, view):
+    def on_activated(self, view):
+        # print("on_activated")
+        self.get_settings()
+        self.update_labels(view)
         sublime.set_timeout(lambda: self.run(view, 'activated'), 0)
 
     def on_selection_modified_async(self, view):
+        # print("on_selection_modified_async")
         sublime.set_timeout(lambda: self.run(view, 'selection_modified'), 0)
 
     def run(self, view, where):
-        # view_settings = view.settings()
-        # if view_settings.get('is_widget'):
-        #     return
-
-        # If we have multiple selections, don't show anything.
         if len(view.sel()) > 1:
             return
         else:
@@ -29,21 +28,48 @@ class KodiTranslatedLabelToolTip(sublime_plugin.EventListener):
 
     def return_label(self, view, selection):
         if selection.isdigit():
-            path, filename = os.path.split(view.file_name())
-            if os.path.exists(os.path.join(path, "resources", "language", "English", "strings.po")):
-                lang_file_path = os.path.join(path, "resources", "language", "English", "strings.po")
-            elif os.path.exists(os.path.join(path, "..", "language", "English", "strings.po")):
-                lang_file_path = os.path.join(path, "..", "language", "English", "strings.po")
-            else:
-                return ""
-            lang_file = open(lang_file_path, "r").read()
-            id_list = re.findall('^msgctxt \"(.*)\"[^\"]*', lang_file, re.MULTILINE)
             id_string = "#" + selection
-            if id_string in id_list:
-                index = id_list.index(id_string)
-                result = re.findall('^msgid \"(.*)\"[^\"]*', lang_file, re.MULTILINE)
-                return result[index + 1]
+            if id_string in self.id_list:
+                index = self.id_list.index(id_string)
+                return self.string_list[index + 1]
         return ""
+
+    def get_settings(self):
+        history_filename = 'KodiTranslateLabels.sublime-settings'
+        history = sublime.load_settings(history_filename)
+        self.kodi_path = history.get("kodi_path")
+        # sublime.save_settings(history_filename)
+
+    def get_addon_lang_file(self, path):
+        if os.path.exists(os.path.join(path, "resources", "language", "English", "strings.po")):
+            lang_file_path = os.path.join(path, "resources", "language", "English", "strings.po")
+        elif os.path.exists(os.path.join(path, "..", "language", "English", "strings.po")):
+            lang_file_path = os.path.join(path, "..", "language", "English", "strings.po")
+        else:
+            return ""
+        return open(lang_file_path, "r").read()
+
+    def get_kodi_lang_file(self, path):
+        if os.path.exists(os.path.join(self.kodi_path, "addons", "resource.language.en_gb", "resources", "strings.po")):
+            lang_file_path = os.path.join(self.kodi_path, "addons", "resource.language.en_gb", "resources", "strings.po")
+        elif os.path.exists(os.path.join(self.kodi_path, "language", "English", "strings.po")):
+            lang_file_path = os.path.join(self.kodi_path, "language", "English", "strings.po")
+        else:
+            return ""
+        return open(lang_file_path, "r").read()
+
+    def update_labels(self, view):
+        path, filename = os.path.split(view.file_name())
+        lang_file = self.get_addon_lang_file(path)
+        self.id_list = re.findall('^msgctxt \"(.*)\"[^\"]*', lang_file, re.MULTILINE)
+        self.string_list = re.findall('^msgid \"(.*)\"[^\"]*', lang_file, re.MULTILINE)
+
+
+class SetKodiFolderCommand(sublime_plugin.TextCommand):
+
+    def run(self, view):
+        sublime.message_dialog("test")
+
 
 
 def jump_to_label_declaration(view, label_id):
