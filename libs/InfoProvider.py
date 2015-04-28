4abf5b7b1d790e5d22d259fffefa4fbccf3fd859
phil65 <philipptemminghoff@gmx.de>
2015-04-28 06:04:15 +0200

    cleanup

--- libs/InfoProvider.py
+++ libs/InfoProvider.py
@@ -8,7 +8,6 @@ class InfoProvider():
 
     def __init__(self):
         self.include_list = []
-        self.var_list = []
         self.include_file_list = []
 
     def get_include_files(self, view):
@@ -16,6 +15,7 @@ class InfoProvider():
             path, filename = os.path.split(view.file_name())
             include_file = os.path.join(path, "Includes.xml")
             if os.path.exists(include_file):
+                log("found include file: " + include_file)
                 parser = ET.XMLParser(remove_blank_text=True)
                 tree = ET.parse(include_file, parser)
                 root = tree.getroot()
@@ -24,6 +24,8 @@ class InfoProvider():
                     if "file" in node.attrib:
                         self.include_file_list.append(os.path.join(path, node.attrib["file"]))
                 # log("File List" + str(self.include_file_list))
+            else:
+                log("Could not find include file")
 
     def update_include_list(self, view):
         self.include_list = []
@@ -33,9 +35,8 @@ class InfoProvider():
 
     def go_to_tag(self, view):
         keyword = findWord(view)
-        goto_list = self.var_list + self.include_list
         if keyword:
-            # log("goto list" + str(goto_list))
-            for node in goto_list:
+            log("include_list" + str(self.include_list))
+            for node in self.include_list:
                 if node["name"] == keyword:
                     sublime.active_window().open_file("%s:%s" % (node["file"], node["line"]), sublime.ENCODED_POSITION)
