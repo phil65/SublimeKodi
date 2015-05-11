8e2d75d884ac00d7cf184ef758e76d68160a7493
Philipp Te <philipptemminghoff@gmx.de>
2015-05-11 17:56:53 +0200

    show alpha for colors

--- sublimekodi.py
+++ sublimekodi.py
@@ -104,14 +104,14 @@ class SublimeKodi(sublime_plugin.EventListener):
                         if item["name"] == selection:
                             color_hex = "#" + item["content"][2:]
                             cont_color = get_cont_col(color_hex)
-                            popup_label += '%s&nbsp;<a style="background-color:%s;color:%s">%s</a><br>' % (os.path.basename(item["filename"]), color_hex + item["content"][0:2], cont_color, item["content"])
+                            alpha_percent = round(int(item["content"][:2], 16) / (16 * 16) * 100)
+                            popup_label += '%s&nbsp;<a style="background-color:%s;color:%s">%s</a> %d %% alpha<br>' % (os.path.basename(item["filename"]), color_hex, cont_color, item["content"], alpha_percent)
                     if not popup_label:
                             if all(c in string.hexdigits for c in selection) and len(selection) == 8:
                                 color_hex = "#" + selection[2:]
                                 cont_color = get_cont_col(color_hex)
-                                int_alpha = int(selection[:2], 16)
-                                alpha_percent = round(int_alpha / (16 * 16) * 100)
-                                popup_label += '<a style="background-color:%s;color:%s">%d %% alpha</a>' % (color_hex + selection[0:2], cont_color, alpha_percent)
+                                alpha_percent = round(int(selection[:2], 16) / (16 * 16) * 100)
+                                popup_label += '<a style="background-color:%s;color:%s">%d %% alpha</a>' % (color_hex, cont_color, alpha_percent)
             elif "<fadetime" in line_contents:
                 popup_label = str(INFOS.return_node_content(findWord(view)))[2:-3]
             elif "<texture" in line_contents or "<alttexture" in line_contents or "<bordertexture" in line_contents or "<icon" in line_contents or "<thumb" in line_contents:
