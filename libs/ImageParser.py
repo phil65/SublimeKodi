d195a18ba5a2ee14b89f528a140c31afc6475980
phil65 <philipptemminghoff@gmx.de>
2015-11-30 23:39:30 +0100

    add ImageParser module

---
+++ libs/ImageParser.py
@@ -0,0 +1,54 @@
+import struct
+import imghdr
+import re
+
+
+def get_image_size(fname):
+    '''Determine the image type of fhandle and return its size.
+    from draco'''
+    with open(fname, 'rb') as fhandle:
+        head = fhandle.read(32)
+        if len(head) != 32:
+            return
+        if imghdr.what(fname) == 'png':
+            check = struct.unpack('>i', head[4:8])[0]
+            if check != 0x0d0a1a0a:
+                return
+            width, height = struct.unpack('>ii', head[16:24])
+        elif imghdr.what(fname) == 'gif':
+            width, height = struct.unpack('<HH', head[6:10])
+        elif imghdr.what(fname) == 'jpeg':
+            try:
+                fhandle.seek(0)  # Read 0xff next
+                size = 2
+                ftype = 0
+                while not 0xc0 <= ftype <= 0xcf:
+                    fhandle.seek(size, 1)
+                    byte = fhandle.read(1)
+                    while ord(byte) == 0xff:
+                        byte = fhandle.read(1)
+                    ftype = ord(byte)
+                    size = struct.unpack('>H', fhandle.read(2))[0] - 2
+                # We are at a SOFn block
+                fhandle.seek(1, 1)  # Skip `precision' byte.
+                height, width = struct.unpack('>HH', fhandle.read(4))
+            except Exception:  # IGNORE:W0703
+                return
+        elif imghdr.what(fname) == 'pgm':
+            header, width, height, maxval = re.search(
+                b"(^P5\s(?:\s*#.*[\r\n])*"
+                b"(\d+)\s(?:\s*#.*[\r\n])*"
+                b"(\d+)\s(?:\s*#.*[\r\n])*"
+                b"(\d+)\s(?:\s*#.*[\r\n]\s)*)", head).groups()
+            width = int(width)
+            height = int(height)
+        elif imghdr.what(fname) == 'bmp':
+            _, width, height, depth = re.search(
+                b"((\d+)\sx\s"
+                b"(\d+)\sx\s"
+                b"(\d+))", str).groups()
+            width = int(width)
+            height = int(height)
+        else:
+            return
+        return width, height
